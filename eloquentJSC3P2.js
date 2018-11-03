91fd1d5350f949baee0fbc4ff0013de13ec81fb2
rp116nj <37672253+rp116nj@users.noreply.github.com>
2018-11-03 07:40:06 -0400

    Add files via upload

---
+++ eloquentJSC3P2.js
@@ -0,0 +1,17 @@
+// Your code here.
+function isEven(n) {
+  n = Math.abs(n);
+  if (n === 0)
+    return true;
+  else if( n === 1)
+    return false;
+  else {
+    return (isEven(n-2));
+  }
+}
+console.log(isEven(50));
+// ? true
+console.log(isEven(75));
+// ? false
+console.log(isEven(-1));
+// ? ??
